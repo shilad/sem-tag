71d32d084ed2d218e7b563a76154a191197aec22
yli2 <yli2@macalester.edu>
2013-07-23 10:54:32 -0500

    Added Field class and constructor in Item class

---
+++ semtag-core/src/main/java/org/semtag/core/Field.java
@@ -0,0 +1,14 @@
+package org.semtag.core;
+
+/**
+ *
+ */
+public class Field {
+    private final int fieldId;
+    private final String fieldName;
+
+    public Field(int fieldId, String fieldName) {
+        this.fieldId = fieldId;
+        this.fieldName = fieldName;
+    }
+}
--- semtag-core/src/main/java/org/semtag/core/Item.java
+++ semtag-core/src/main/java/org/semtag/core/Item.java
@@ -1,7 +1,30 @@
 package org.semtag.core;
 
+import java.util.Map;
+
 /**
  * @author Ari Weiland
  */
 public class Item {
+    private final int itemId;
+    private final String itemName;
+    private final Map<String, Field> properties;
+
+    public Item(int itemId, String itemName, Map<String, Field> properties) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.properties = properties;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public Map<String, Field> getProperties() {
+        return properties;
+    }
 }
