2b941d1a632029cf7b886d459dfde2f9e7124869
yli2 <yli2@macalester.edu>
2013-07-23 13:11:57 -0500

    Updated concept

--- semtag-core/src/main/java/org/semtag/core/Concept.java
+++ semtag-core/src/main/java/org/semtag/core/Concept.java
@@ -4,4 +4,19 @@ package org.semtag.core;
  *
  */
 public class Concept {
+    private final int conceptId;
+    private final String planText;
+
+    public Concept(int conceptId, String planText) {
+        this.conceptId = conceptId;
+        this.planText = planText;
+    }
+
+    public int getConceptId() {
+        return conceptId;
+    }
+
+    public String getPlanText() {
+        return planText;
+    }
 }
