658d094f1cdbb74c21d197744aaece4badabe470
yli2 <yli2@macalester.edu>
2013-07-24 11:40:14 -0500

    edited tagapploader

--- semtag-core/src/main/java/org/semtag/loader/TagAppLoader.java
+++ semtag-core/src/main/java/org/semtag/loader/TagAppLoader.java
@@ -1,7 +1,26 @@
 package org.semtag.loader;
 
+import org.semtag.core.dao.TagAppDao;
+import org.semtag.core.model.TagApp;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
+
 /**
  *
  */
 public class TagAppLoader {
+
+    private static final Logger LOG = Logger.getLogger(TagAppLoader.class.getName());
+    private final AtomicInteger counter = new AtomicInteger();
+    private final TagAppDao tagAppDao;
+
+    public TagAppLoader(TagAppDao tagAppDao) {
+        this.tagAppDao = tagAppDao;
+    }
+
+    public void add(TagApp tagApp) {
+
+    }
+
 }
