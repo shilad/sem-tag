ddfbd9ccc5a6cea183148a4019b733ac6f5e9d15
Ari Weiland <math1man@gmail.com>
2013-07-25 15:55:34 -0500

    more TagApp concept things

--- semtag-core/src/main/java/org/semtag/core/model/TagApp.java
+++ semtag-core/src/main/java/org/semtag/core/model/TagApp.java
@@ -1,5 +1,7 @@
 package org.semtag.core.model;
 
+import org.semtag.core.dao.ConceptDao;
+import org.semtag.core.dao.DaoException;
 import org.semtag.core.model.concept.Concept;
 
 import java.sql.Timestamp;
@@ -13,9 +15,9 @@ public class TagApp {
     private final Tag tag;
     private final Item item;
     private final Timestamp timestamp;
+    private final int conceptId;
 
     private Concept concept;
-    private int conceptId;
 
     public TagApp(long tagAppId, User user, Tag tag, Item item, Timestamp timestamp, int conceptId) {
         this.tagAppId = tagAppId;
@@ -24,6 +26,7 @@ public class TagApp {
         this.item = item;
         this.timestamp = timestamp;
         this.conceptId = conceptId;
+        this.concept = null;
     }
 
     public TagApp(long tagAppId, User user, Tag tag, Item item, Timestamp timestamp, Concept concept) {
@@ -32,8 +35,8 @@ public class TagApp {
         this.tag = tag;
         this.item = item;
         this.timestamp = timestamp;
-        this.concept = concept;
         this.conceptId = concept.getConceptId();
+        this.concept = concept;
     }
 
     public long getTagAppId() {
@@ -64,9 +67,33 @@ public class TagApp {
         return concept;
     }
 
-    public void setConcept(Concept concept) {
-        this.concept = concept;
-        this.conceptId = concept.getConceptId();
+    /**
+     * Sets the concept only if the specified concept matches this TagApp's concept ID,
+     * and the concept was not previously set. Returns whether or not the concept was set.
+     * @param concept
+     * @return true only if the concept was not previously set and the input concept is valid
+     */
+    public boolean setConcept(Concept concept) {
+        if (concept != null && this.concept == null && conceptId == concept.getConceptId()) {
+            this.concept = concept;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Sets the concept to the concept retrieved by the specified ConceptDao, as long as
+     * the concept was not previously set. Returns whether or not the concept was set.
+     * @param dao
+     * @return true only if the concept was not previously set and the dao returned a valid concept
+     * @throws DaoException
+     */
+    public boolean setConcept(ConceptDao dao) throws DaoException {
+        if (concept == null) {
+            concept = dao.getByConceptId(conceptId);
+            return concept != null;
+        }
+        return false;
     }
 
     @Override
