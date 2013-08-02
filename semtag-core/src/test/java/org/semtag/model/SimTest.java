0a614297c20eaf6d6c94eee55866a083ea2647c6
Ari Weiland <math1man@gmail.com>
2013-08-02 11:15:42 -0500

    ...

--- semtag-core/src/test/java/org/semtag/model/SimTest.java
+++ semtag-core/src/test/java/org/semtag/model/SimTest.java
@@ -3,7 +3,8 @@ package org.semtag.model;
 import org.junit.Test;
 import org.semtag.dao.ConceptDao;
 import org.semtag.dao.DaoException;
-import org.semtag.model.concept.WikapidiaConcept;
+import org.semtag.dao.DaoFilter;
+import org.semtag.model.concept.Concept;
 import org.semtag.sim.ConceptSimilarity;
 import org.semtag.sim.SimilarResult;
 import org.semtag.sim.SimilarResultList;
@@ -11,8 +12,10 @@ import org.wikapidia.conf.Configuration;
 import org.wikapidia.conf.ConfigurationException;
 import org.wikapidia.conf.Configurator;
 import org.wikapidia.core.lang.Language;
-import org.wikapidia.core.lang.LocalId;
 import org.wikapidia.sr.LocalSRMetric;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Ari Weiland
@@ -27,12 +30,23 @@ public class SimTest {
         ConceptDao dao = conf.get(ConceptDao.class);
         ConceptSimilarity sim = conf.get(ConceptSimilarity.class);
         LocalSRMetric metric = conf.get(LocalSRMetric.class);
-        WikapidiaConcept c1 = new WikapidiaConcept(new LocalId(LANGUAGE, 223430), metric); // Barack Obama
-        WikapidiaConcept c2 = new WikapidiaConcept(new LocalId(LANGUAGE, 219587), metric); // United States
-        System.out.println("Similarity: " + sim.similarity(c1, c2));
-        SimilarResultList list = sim.mostSimilar(c1, 10);
-        for (SimilarResult result : list) {
-            System.out.println(dao.getByConceptId(result.getIntId()) + " : " + result.getValue());
+        List<Concept> concepts = new ArrayList<Concept>();
+        Iterable<Concept> iterable = dao.get(new DaoFilter());
+        int i=0;
+        for (Concept concept : iterable) {
+            if (concept.getConceptId() > -1) {
+                concepts.add(concept);
+                i++;
+            }
+            if (i>=10) break;
+        }
+        Concept x = concepts.get(0);
+        for (Concept y : concepts) {
+            System.out.println(x.getConceptObj() + " to " + y.getConceptObj() + " : " + sim.similarity(x, y));
+            SimilarResultList list = sim.mostSimilar(y, 10);
+            for (SimilarResult result : list) {
+                System.out.println("Most sim to " + y.getConceptObj() + ": " + dao.getByConceptId(result.getIntId()).getConceptObj() + " - " + result.getValue());
+            }
         }
     }
 }
