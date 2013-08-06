a751250c61ec5a3ee5f0ba338cbbdf5a73387be5
Ari Weiland <math1man@gmail.com>
2013-08-06 15:28:32 -0500

    tests straightened out

--- semtag-core/src/test/java/org/semtag/model/TestTag.java
+++ semtag-core/src/test/java/org/semtag/model/TestTag.java
@@ -11,6 +11,8 @@ public class TestTag {
     public void testNormalize() {
         String tag = "  ase5 h34[qi   34%#PIa_wkr\tp34j5-poa ijf!\n";
         System.out.println(tag);
-        System.out.println(Tag.normalize(tag)+"*");
+        String normalized = Tag.normalize(tag);
+        System.out.println(normalized+"*");
+        assert (normalized.equals("ase5 h34 qi 34 pia wkr p34j5 poa ijf"));
     }
 }
